54f9ffe9aad9ac90e8469faf7972304f1e4ea747
j721 <juliadvu@gmail.com>
2020-10-19 23:15:13 -0500

    updated start scripts and initial imports for server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
+
+const app = express();
