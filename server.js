5026b63249915732209b2552f9da33eecbfca47b
j721 <juliadvu@gmail.com>
2020-10-19 23:41:08 -0500

    User type created

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql").graphqlHTTP;
 const {
+  buildSchema,
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
   GraphQLList,
   GraphQLInt,
   GraphQLNonNull,
+  graphql,
 } = require("graphql");
 
 const app = express();
+
+const schema = buildSchema(
+  `
+     type Query{
+      users: User
+    }
+    type User{
+      id: ID!         //exclamation point stands for non-nullable. Every user has to have an id
+      email: String!
+      name:String
+      avatarUrl: String
+    }
+  `
+);
+
+const rootValue = {
+  message: () => "GraphQl works",
+};
+
+graphql(
+  schema,
+  `
+    {
+      message
+    }
+  `,
+  rootValue
+)
+  .then(console.log)
+  .catch(console.error);
